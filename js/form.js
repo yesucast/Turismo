a780b552cc4d46180469f9b5f7dc7a4c3127a933
yesucast <62528008+yesucast@users.noreply.github.com>
2020-03-22 23:20:47 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -6,7 +6,7 @@ document.querySelector('#submit').addEventListener('click',function(){
     let dia = document.querySelector('#dia').value;
     let servicio = document.querySelector('#servicio').value;
 
-    let url = "https://api.whatsapp.com/send?phone=51931512715&text=*_Peruvian%20Crossings_*%20%0A*Reservas*%0A%0A*¿Cuál%20%es%20%tu%20%nombre?*%0A" + cliente + "%0A*Indica%20%la%20%fecha%20%de%20%tu%20%reserva*%0A" + fecha + "%0A*Indica%20%la%20%hora%20%de%20%tu%20%reserva*%0A" + hora + "%0A*Día%20%de%20%preferencia*%0A" + dia + "%0A*¿Cuál%20%es%20%el%20%tour%20%que%20%desea%20%realizar?*%0A" + servicio;
+    let url = "https://api.whatsapp.com/send?phone=51931512715&text=*_Peruvian%20Crossings_*%20%0A*Reservas*%0A%0A*¿Cuál%20es%20tu%20nombre?*%0A" + cliente + "%0A*Indica%20la%20fecha%20de%20tu%20reserva*%0A" + fecha + "%0A*Indica%20la%20hora%20de%20tu%20reserva*%0A" + hora + "%0A*Día%20de%20preferencia*%0A" + dia + "%0A*¿Cuál%20es%20el%20tour%20que%20desea%20realizar?*%0A" + servicio;
     window.open(url);
 
 });
